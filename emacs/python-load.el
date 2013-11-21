676ee981d8163f9a7c21e1febb7d6cc89439b8b3
petergardfjall <peter.gardfjall.work@gmail.com>
2013-11-21 15:06:05 +0100

    updated python mode to gallina's python.el

---
+++ emacs/python-load.el
@@ -0,0 +1,10 @@
+;
+; Uses the emacs23 branch of the "gallina python.el" python mode code which 
+; is now *included* in Emacs 24 (see https://github.com/fgallina/python.el).
+; For more details on its use, refer to:
+;   http://www.emacswiki.org/emacs/ProgrammingWithPythonDotEl
+;
+(add-to-list 'load-path "~/dotfiles/emacs/python.el")
+(require 'python)
+; bind RETURN to "newline followed by indent", rather than "newline only".
+(add-hook 'python-mode-hook '(lambda () (define-key python-mode-map "\C-m" 'newline-and-indent)))
--- emacs/python-mode-load.el
+++
@@ -1,4 +0,0 @@
-(add-to-list 'load-path "~/dotfiles/emacs/python-mode.el-6.1.1/")
-(autoload 'python-mode "python-mode" "Python Mode." t)
-(add-to-list 'auto-mode-alist '("\\.py\\'" . python-mode))
-(add-to-list 'interpreter-mode-alist '("python" . python-mode))
