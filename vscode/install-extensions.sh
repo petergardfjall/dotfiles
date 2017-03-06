40c5167f5fdc29d5ced3b7e33392ee4c41460b8a
petergardfjall <peter.gardfjall.work@gmail.com>
2017-03-06 19:51:53 +0100

    add vscode dotfiles

---
+++ vscode/install-extensions.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Go mode
+code --install-extension lukehoban.Go
+# Python mode
+code --install-extension donjayamanne.python
+# Dockerfile mode
+code --install-extension PeterJausovec.vscode-docker
+# Java language support
+code --install-extension redhat.java
+
+# icon theme
+code --install-extension PKief.material-icon-theme
