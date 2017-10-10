7e8f0c2a2c6439be7ccdd9be98e56436efdd907f
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2017-10-10 07:45:19 +0200

    vscode: added subword navigation plugin and keybindings ('camel-humps navigation')

--- vscode/install-extensions.sh
+++ vscode/install-extensions.sh
@@ -8,6 +8,11 @@ code --install-extension donjayamanne.python
 code --install-extension PeterJausovec.vscode-docker
 # Java language support
 code --install-extension redhat.java
+# TOML support
+code --install-extension bungcip.better-toml
 
 # icon theme
-#code --install-extension PKief.material-icon-theme
+code --install-extension PKief.material-icon-theme
+
+# subword navigation (jumping between upcase letters in camel-case words)
+code --install-extension ow.vscode-subword-navigation
