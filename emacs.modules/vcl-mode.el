5bf7baa35390ce0fa5beb6de3fc4e0e49da251a8
petergardfjall <peter.gardfjall.work@gmail.com>
2019-01-12 17:30:22 +0100

    emacs-init: use vcl-mode (varnish) from official archives

--- emacs.modules/vcl-mode.el
+++
@@ -1,7 +0,0 @@
-;;
-;; Syntax highlightning for Varnish Configuration Languave (VCL) buffers
-;;
-(add-to-list 'load-path "~/dotfiles/emacs.modules/emacs-vcl-mode/")
-(require 'vcl-mode)
-(add-to-list 'auto-mode-alist '("\\.vcl\\'" . vcl-mode))
-(setq vcl-indent-level 4)
