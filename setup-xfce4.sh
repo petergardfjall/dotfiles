b8b74e3ff67e45c285c53462689e338a48d8091b
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2019-02-05 10:42:05 +0100

    setup-xfce4: link file-by-file and not entire dir, also restart xfconfd to prevent it from overwriting xfce4-panel.xml

--- setup-xfce4.sh
+++ setup-xfce4.sh
@@ -2,6 +2,16 @@
 
 set -e
 
+ts=$(date +%Y%m%d-%H%M)
+
+function info () {
+    echo -e "\e[32m${1}\e[0m"
+}
+
+function warn () {
+    echo -e "\e[32m${1}\e[0m"
+}
+
 #
 # Links xfce4 settings files from ~/.config/xfce4/
 # Any existing files are backed up under ~/.config/xfce4/backup.
@@ -14,23 +24,42 @@ dest=~/.config/xfce4
 backup=~/.config/xfce4/backup
 
 mkdir -p ${dest}
+mkdir -p ${backup}
 
-for file in $(ls -1 ~/dotfiles/xfce4); do
+# Kill the running xfconfd since otherwise it will overwrite our custom
+# xfce4-panel.xml with the session panel. We'll restart it at the end of the
+# script by restarting the xfce4-panel.
+warn "temporarily killing xfconfd ..."
+pkill -KILL xfconfd
+
+# for each flie in ~/dotfiles/xfce4/, make a symlink to it from
+# ~/.config/xfce4
+for file in $(cd ${src} && find -type f | sed 's#^./##'); do
     if [ -h ${dest}/${file} ]; then
-	echo "[WARN] symlink already exists for ${dest}/${file}. Ignoring ..."
+	info "symlink already exists for ${dest}/${file}. Ignoring ..."
 	continue
     fi
 
     if [ -e ${dest}/${file} ]; then
 	# destination exists => take backup
-	echo "[INFO] ${file} exists in ${dest}. Moving to ${backup} ..."
-	mkdir -p ${backup}
-	mv ${dest}/${file} ${backup}/${file}
+	warn "${file} file exists in ${dest}. Moving to ${backup} ..."
+	backup_path="${backup}/${ts}-${file}"
+	# ensure backup directory exists
+	backup_dir=$(dirname ${backup_path})
+	mkdir -p ${backup_dir}
+	mv ${dest}/${file} ${backup_path}
     fi
 
-    echo "[INFO] linking ${dest}/${file} => ${src}/${file} ..."
-    ln -s ${src}/${file} ${dest}/${file}
+    destpath="${dest}/${file}"
+    info "linking ${destpath} => ${src}/${file} ..."
+    # ensure destination directory exists
+    destdir=$(dirname ${destpath})
+    mkdir -p ${destdir}
+    ln -s ${src}/${file} ${destpath}
 done
 
-echo "[INFO] setting up xfce keyboard shortcuts ..."
+info "restarting xfce4-panel (also starts xfconfd) ..."
+xfce4-panel --restart
+
+echo "setting up xfce keyboard shortcuts ..."
 ${scriptdir}/setup-xfce4-keyboard-shortcuts.sh
