de01d709265ee7a88c660a25347642cb66dd34db
petergardfjall <peter.gardfjall.work@gmail.com>
2019-02-09 17:23:22 +0100

    emacs-init: make use of github page for storing custom js/css for markdown-preview-mode

--- emacs.modules/markdown-preview-styling/github-markdown-block-highlight.js
+++
@@ -1,7 +0,0 @@
-<script>
-    $(document).on('mdContentChange', function() {
-        $('pre code').each(function(i, block) {
-            hljs.highlightBlock(block);
-        });
-    });
-</script>
