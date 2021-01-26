40951364809ff1e229a24dad6e05edc82b1f144c
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2021-01-26 09:21:04 +0100

    xfce: super+arrow to move mouse (additional modifier keys to slow down)
    super+{,.} to click.

--- setup-xfce4-keyboard-shortcuts.sh
+++ setup-xfce4-keyboard-shortcuts.sh
@@ -35,21 +35,29 @@ done
 <Alt>F12           | above_key
 Escape             | cancel_key
 <Alt><Space>       | popup_menu_key
+#
 # Alt+Tab and Alt+Shift+Tab => cycle windows in workspace
+#
 <Alt>Tab           | cycle_windows_key
 <Alt><Shift>Tab    | cycle_reverse_windows_key
+#
 # Ctrl+Alt+arrow => move to a different workspace
+#
 <Primary><Shift><Alt>M | move_window_key
 <Primary><Alt>Right    | right_workspace_key
 <Primary><Alt>Left     | left_workspace_key
 <Primary><Alt>Up       | up_workspace_key
 <Primary><Alt>Down     | down_workspace_key
+#
 # Ctrl+Shift+arrow => move window to a different workspace
+#
 <Primary><Shift><Alt>Right    | move_window_right_workspace_key
 <Primary><Shift><Alt>Left     | move_window_left_workspace_key
 <Primary><Shift><Alt>Up       | move_window_up_workspace_key
 <Primary><Shift><Alt>Down     | move_window_down_workspace_key
+#
 # Ctrl+Shift+<keypad> => tile window in different directions
+#
 <Primary><Shift>7    |  tile_up_left_key
 <Primary><Shift>4    |  tile_left_key
 <Primary><Shift>6    |  tile_right_key
@@ -58,8 +66,12 @@ Escape             | cancel_key
 <Primary><Shift>9    |  tile_up_right_key
 <Primary><Shift>1    |  tile_down_left_key
 <Primary><Shift>3    |  tile_down_right_key
-# Ctrl+Shift+<keypad5> twice will move a tiled window to its original position
-<Primary><Shift>5    |  maximize_vert_key
+#
+# Ctrl+Shift+<keypad5> twice will move a tiled window to its original
+# position
+#
+<Primary><Shift>5      |  maximize_vert_key
+<Primary><Shift><Alt>M | move_window_key
 EOF
 ) | while read row; do
     if echo ${row} | egrep -q '^[ ]*#'; then
@@ -86,19 +98,34 @@ done
 <Primary><Alt>Delete  |  xflock4
 <Primary><Alt>l       |  xflock4
 <Primary><Alt>t       |  xfce4-terminal
-Super_L               |  xfce4-appfinder
-<Shift><Alt>Left      |  xdotool mousemove_relative -- -20 0
-<Shift><Alt>Right     |  xdotool mousemove_relative -- 20 0
-<Shift><Alt>Up        |  xdotool mousemove_relative -- -0 -20
-<Shift><Alt>Down      |  xdotool mousemove_relative -- 0 20
-# Left mouse click
-<Shift><Alt>Return    |  xdotool keyup alt+Shift_L click 1
-# Right mouse click
-<Shift><Alt>Backspace |  xdotool keyup alt+Shift_L click 3
+<Super>a              |  xfce4-appfinder
 # Full screen screenshot
 <Alt>Insert           | xfce4-screenshooter -f
 # Focused window screenshot
 <Primary><Alt>Insert  | xfce4-screenshooter -w
+#
+# Mouse pointer movement with Super+arrow. Additional modifier keys
+# to move slower:
+# Super+arrow            => move mouse pointer (long)
+# Super+Alt+arrow        => move mouse pointer (medium)
+# Super+Alt+Ctrl+arrow   => move mouse pointer (short)
+# <Super>+,              => left mouse click
+# <Super>+.              => right mouse click
+#
+<Super>Left                | xdotool mousemove_relative -- -120 0
+<Super>Right               | xdotool mousemove_relative --  120 0
+<Super>Up                  | xdotool mousemove_relative --  0 -120
+<Super>Down                | xdotool mousemove_relative --  0  120
+<Super><Alt>Left           | xdotool mousemove_relative -- -40 0
+<Super><Alt>Right          | xdotool mousemove_relative --  40 0
+<Super><Alt>Up             | xdotool mousemove_relative --  0 -40
+<Super><Alt>Down           | xdotool mousemove_relative --  0  40
+<Super><Alt><Primary>Left  | xdotool mousemove_relative -- -10 0
+<Super><Alt><Primary>Right | xdotool mousemove_relative --  10 0
+<Super><Alt><Primary>Up    | xdotool mousemove_relative --  0 -10
+<Super><Alt><Primary>Down  | xdotool mousemove_relative --  0  10
+<Super>comma               | xdotool click 1
+<Super>period              | xdotool click 3
 EOF
 ) | while read row; do
     if echo ${row} | egrep -q '^[ ]*#'; then
