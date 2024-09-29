02d2de3e21abd3a05b0d1b1b578776663e9426a0
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2024-09-29 11:03:29 +0200

    setup-xfce4.sh: simplify script
    - Now uses xfce4-panel-profiles to restore the xfce4-panel.
    - Applies saved terminal settings through one saved xfce4-terminal.xml file.
    - No longer creates symlinks under ~/.config/xfce

--- setup-xfce4.sh
+++ setup-xfce4.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-set -e
+here=$(dirname $(realpath ${0}))
 
-ts=$(date +%Y%m%d-%H%M)
+set -e
 
 function info () {
     echo -e "\e[32m${1}\e[0m"
@@ -13,59 +13,22 @@ function warn () {
 }
 
 #
-# Links xfce4 settings files from ~/.config/xfce4/
-# Any existing files are backed up under ~/.config/xfce4/backup.
+# Apply saved xfce4-panel profile.
+# - A profile is exported via: "xfce4-panel-profiles save profile.tar.bz2"
 #
+info "applying xfce4-panel profile ..."
+# Note: for the time being this appears to kill the panel due to [1].
+#       Should this happen just try 'xfce4-panel &' from a terminal.
+# [1] https://bugs.launchpad.net/ubuntu/+source/xfce4-panel/+bug/2064846
+xfce4-panel-profiles load ${here}/xfce4/xfce4-panel-profile.tar.bz2
 
-scriptdir=$(dirname ${0})
-
-src=~/dotfiles/xfce4
-dest=~/.config/xfce4
-backup=~/.config/xfce4/backup
-
-mkdir -p ${dest}
-mkdir -p ${backup}
-
-# Kill the running xfconfd since otherwise it will overwrite our custom
-# xfce4-panel.xml with the session panel. We'll restart it at the end of the
-# script by restarting the xfce4-panel.
-warn "temporarily killing xfconfd ..."
-pkill xfconfd || warn "xfconfd does not appear to be running ..."
-
-# for each flie in ~/dotfiles/xfce4/, make a symlink to it from
-# ~/.config/xfce4
-for file in $(cd ${src} && find -type f | sed 's#^./##'); do
-    if [ -h ${dest}/${file} ]; then
-        info "symlink already exists for ${dest}/${file}. Ignoring ..."
-        continue
-    fi
-
-    if [ -e ${dest}/${file} ]; then
-        # destination exists => take backup
-        warn "${file} file exists in ${dest}. Moving to ${backup} ..."
-        backup_path="${backup}/${ts}-${file}"
-        # ensure backup directory exists
-        backup_dir=$(dirname ${backup_path})
-        mkdir -p ${backup_dir}
-        mv ${dest}/${file} ${backup_path}
-    fi
-
-    destpath="${dest}/${file}"
-    info "linking ${destpath} => ${src}/${file} ..."
-    # ensure destination directory exists
-    destdir=$(dirname ${destpath})
-    mkdir -p ${destdir}
-    ln -s ${src}/${file} ${destpath}
-done
-
-info "restarting xfce4-panel (should restart xfconfd) ..."
-# may not be possible when not in X11 mode
-xfce4-panel --restart || warn "could not restart xfce4-panel, not in graphical mode?"
-# this should take care of restarting xfcond when not in X11 mode
-xfconf-query --list > /dev/null 2>&1
+#
+# Apply saved terminal settings.
+#
+cp ${here}/xfce4/xfce4-terminal.xml ~/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-terminal.xml
 
 echo "setting up xfce keyboard shortcuts ..."
-${scriptdir}/setup-xfce4-keyboard-shortcuts.sh
+${here}/setup-xfce4-keyboard-shortcuts.sh
 
 echo "applying some general xfce settings ..."
 # Disable not zoom desktop on Alt+mousewheel.
