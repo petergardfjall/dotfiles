e9b35b6089361639d075983a666adab68063a891
Peter Gardfjäll <peter.gardfjall.work@gmail.com>
2024-11-09 10:27:56 +0100

    xfce4-hidpi.sh: no need to resize mouse cursor

--- xfce4-hidpi.sh
+++ xfce4-hidpi.sh
@@ -24,14 +24,10 @@ if ${enable}; then
     xfconf-query -c xsettings -p /Gdk/WindowScalingFactor -s 2
     # Use a high-dpi friendly window manager theme.x
     xfconf-query -c xfwm4 -p /general/theme -s Default-hdpi
-    # Scale mouse cursor: 24 -> 48 pixels (> Mouse > Theme).
-    xfconf-query -c xsettings -p /Gtk/CursorThemeSize -s 48
 else
     echo "Disabling High-DPI settings ..."
     # Set normal window scaling factor x1 (> Appearance > Settings).
     xfconf-query -c xsettings -p /Gdk/WindowScalingFactor -s 1
     # Use default window manager theme.
     xfconf-query -c xfwm4 -p /general/theme -s Default
-    # Default size mouse cursor: 24 pixels (> Mouse > Theme).
-    xfconf-query -c xsettings -p /Gtk/CursorThemeSize -s 24
 fi
